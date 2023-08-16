a479b76a3d5250596208e21d132fe5c9f0b22306
donnie <donnie4w@gmail.com>
2023-08-16 23:12:14 +0800

    Update client_test.go

--- client/client_test.go
+++ client/client_test.go
@@ -5,12 +5,13 @@ import (
 
 	"github.com/donnie4w/simplelog/logging"
 	"github.com/donnie4w/tlcli-go/tlcli"
+	. "github.com/donnie4w/tlcli-go/tlcli"
 )
 
 func Test_client(t *testing.T) {
-	if client, err := tlcli.NewConnect(false, "db.tlnet.top:7001", "mycli=123"); err == nil {
+	if client, err := NewConnect(false, "db.tlnet.top:7001", "mycli=123"); err == nil {
 		//create table
-		client.CreateTable("usertest", []string{"name", "age", "type"}, []string{"name", "type"})
+		client.CreateTable("usertest", map[string]COLUMNTYPE{"name": STRING, "age": STRING, "type": STRING}, []string{"name", "type"})
 		//insert data
 		datamap := map[string][]byte{"name": []byte("tom"), "age": []byte("1"), "type": []byte("1")}
 		seq, _ := client.Insert("usertest", datamap)
@@ -26,7 +27,7 @@ func Test_client(t *testing.T) {
 func Test_clientTls(t *testing.T) {
 	if client, err := tlcli.NewConnect(false, "db.tlnet.top:7002", "mycli=123"); err == nil {
 		//create table
-		client.CreateTable("usertest", []string{"name", "age", "type"}, []string{"name", "type"})
+		client.CreateTable("usertest", map[string]COLUMNTYPE{"name": STRING, "age": STRING, "type": STRING}, []string{"name", "type"})
 		//insert data
 		datamap := map[string][]byte{"name": []byte("jerry"), "age": []byte("2"), "type": []byte("2")}
 		seq, _ := client.Insert("usertest", datamap)
